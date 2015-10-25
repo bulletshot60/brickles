8e05db55687b0476e191a01ce4598744040fc101
Ethan McGee <ethan.mcgee@m33integrated.com>
2015-10-25 16:04:12 -0400

    cleanup and add info

--- edu/clemson/cs/test/unit/BrickPileTests.java
+++
@@ -1,8 +0,0 @@
-package edu.clemson.cs.test.unit;
-
-import static org.junit.Assert.*;
-import org.junit.Test;
-
-public class BrickPileTests {
-
-}
--- edu/clemson/cs/test/unit/BricklesGameTests.java
+++ edu/clemson/cs/test/unit/BricklesGameTests.java
@@ -3,6 +3,51 @@ package edu.clemson.cs.test.unit;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
-public class BricklesGameTests {
+import edu.clemson.cs.BricklesGame;
+import edu.clemson.cs.BricklesView;
 
+public class BricklesGameTests {
+	public BricklesGame initGame() {
+		BricklesView view = new BricklesView();
+		view.init();
+		view.start();
+		return view.getGame();
+	}
+	
+	@Test
+	public void testConstructor() {
+		BricklesGame game = initGame();
+		new BricklesGame(game.getView());
+		assertTrue(true); // no exception thrown
+	}
+	
+	@Test
+	public void testGetView() {
+		BricklesGame game = initGame();
+		assertTrue(game.getView() instanceof BricklesView);
+	}
+	
+	@Test
+	public void testWon() {
+		BricklesGame game = initGame();
+		assertTrue(game.isLost() == false);
+		assertTrue(game.isOver() == false);
+		assertTrue(game.isWon() == false);
+		game.won();
+		assertTrue(game.isLost() == false);
+		assertTrue(game.isOver() == true);
+		assertTrue(game.isWon() == true);
+	}
+	
+	@Test 
+	public void testLost() {
+		BricklesGame game = initGame();
+		assertTrue(game.isLost() == false);
+		assertTrue(game.isOver() == false);
+		assertTrue(game.isWon() == false);
+		game.lost();
+		assertTrue(game.isLost() == true);
+		assertTrue(game.isOver() == true);
+		assertTrue(game.isWon() == false);
+	}
 }
